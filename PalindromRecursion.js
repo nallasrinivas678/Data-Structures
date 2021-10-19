382e0ac3eb1d46521fb21553ae59d501f038b573
srinivas <nallasrinivas678@gmail.com>
2021-10-18 21:46:28 -0400

    Update PalindromRecursion.js

--- PalindromRecursion.js
+++ PalindromRecursion.js
@@ -1,7 +1,7 @@
 //to check if given string is palindrome or not
 function isPalindrome(str){
   if(str.length === 1) return true; //if length is 1 
-  if(str.length === 1) return str[0] === str[1]; //if string is only 2 characters, check if they are equal
+  if(str.length === 2) return str[0] === str[1]; //if string is only 2 characters, check if they are equal
   
    //if first and last letters are equal str.sice(-1) returns last character
    //str.slice(1,-1) remove first and last character and returns remaing string
