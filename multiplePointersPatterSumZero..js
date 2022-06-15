ec9e6c67639013e11c1b83de0ccbc32237ad2241
srinivas <nallasrinivas678@gmail.com>
2022-06-15 14:24:04 -0500

    Update multiplePointersPatterSumZero..js

--- multiplePointersPatterSumZero..js
+++ multiplePointersPatterSumZero..js
@@ -32,7 +32,7 @@ const sumZeroNaiveSoltuion = (arr) => {
 
     for(let i =0; i< arr.length; i++){
         
-        for(j=i; j< arr.length; j++){
+        for(j=i+1; j< arr.length; j++){
             if(arr[i]+arr[j] === 0) return [arr[i], arr[j]];
             
         }
