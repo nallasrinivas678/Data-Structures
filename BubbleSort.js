f081117017b19e669c74df70e3a4d2465d8dc82b
srinivas <nallasrinivas678@gmail.com>
2022-08-25 22:46:04 -0400

    Update BubbleSort.js

--- BubbleSort.js
+++ BubbleSort.js
@@ -24,5 +24,35 @@ function BubbleSort(arr){
     return arr;
 }
 
+//Alternate sln
+function bubbleSort(array) {
+  
+  let isSorted = false;
+  let counter = 0; 
+    //counter: always last value in the array is sorted, so to avoid comparing those values use counter and
+    //stop the loop at the counter
+
+  while(!isSorted){
+    isSorted = true;
+    for(let i =0; i < array.length-1-counter; i++){
+      if(array[i] >= array[i+1]){
+       array = swap(i, i+1, array);
+        isSorted = false;
+      }
+    }
+    counter++;
+  }
+
+  return array;
+  
+}
+
+function swap(i,j, array){
+ let temp = array[j];
+  array[j] = array[i];
+  array[i] = temp;
+  return array;
+}
+
 
 BubbleSort([8,1,2,3,4,5,6,7])
