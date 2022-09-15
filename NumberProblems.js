9b3ce3dcf390ff5ff8fc341ecc5ace9ca649d2db
srinivas <nallasrinivas678@gmail.com>
2022-09-15 15:45:56 -0400

    Update NumberProblems.js

--- NumberProblems.js
+++ NumberProblems.js
@@ -1,1 +1,15 @@
 
+//Notes: use modulo opertaor to get remainer.
+//Reverse a number 123 - 321
+function reverseNumber(num){
+    var revNum = 0;
+    var n = num;
+    while(n != 0){
+        rem = n % 10;
+        revNum =  10 * revNum + rem;
+        n = Math.floor(n/10);
+    }
+    return revNum;
+}
+
+console.log(reverseNumber(12345));
