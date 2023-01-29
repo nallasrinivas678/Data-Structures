b79226a8b8e2548155b2beb528a9c7514774860a
srinivas <nallasrinivas678@gmail.com>
2023-01-28 20:58:52 -0500

    Added hasPath problem

--- GraphTraversalProblems.js
+++ GraphTraversalProblems.js
@@ -33,6 +33,44 @@ const breadthFirstSearch = (graph, sourceNode) => {
     }
 }
 
+const hasPathRecursvive = (graph, src, dest) => {
+
+  if(src === dest ) return true;
+
+  //idea is to navigate through neighbors 
+  //and find out if any neighbor has path to dest
+  //which means it has path from src also
+  for(let neighbor of graph[src]){
+  if(hasPath(graph, neighbor, dest) === true)
+    return true;
+  }
+
+  //if we didnt find any path
+  return false;
+}
+
+const hasPath = (graph, src, dst) => {
+  //If source and des are same return true here
+    if(src === dst) return true;
+
+    const stack = [src];
+
+    while(stack.length > 0){
+
+        const current = stack.pop();
+
+        for(let neighbor of graph[current]){
+            stack.push(neighbor);
+            if(neighbor === dst) return true;
+        }
+        
+    }
+
+    return false;
+}
+    
+  
+
 
 const graph = {
     a: ['c', 'b'],
@@ -41,6 +79,16 @@ const graph = {
     d: ['f'],
     e: [],
     f: []
+}
+
+//acyclic graph - for hasPath
+const graph2 = {
+    f: ['g', 'i'],
+    g: ['h'],
+    h: [],
+    i: ['g', 'k'],
+    j: ['i'],
+    k: []
 }
 
 depthFirstSearch(graph, 'a');
