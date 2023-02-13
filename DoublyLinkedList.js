d798538939f6b6523830e6f5d5ac6da30ddfd571
srinivas <nallasrinivas678@gmail.com>
2023-02-13 10:48:36 -0500

    Completed all methods in DLL

--- DoublyLinkedList.js
+++ DoublyLinkedList.js
@@ -8,6 +8,10 @@ class Node{
 }
 
 class DoublyLinkedList {
+    //Big 0
+    //Insertion - O(1)
+     //Removal - O(1)
+     //Searching - O(N)
     constructor(){
         this.head = null;
         this.tail = null;
@@ -124,11 +128,89 @@ class DoublyLinkedList {
         return current;
         
     }
+
+    
+
+    set(index,val){
+
+        var node = this.get(index);
+        if(node !== null){
+             node.val = val;
+             return true;
+        }
+        return false;
+        
+    }
+
+    insert(index, val){
+        //1.check if index is valid (index >0 && <= length || == length) 
+
+        var newNode = new Node(val);
+        
+        if(index < 0 || index > this.length)
+            return false;
+        if(index === 0)
+        {
+            //use unshift - add at begining
+            //Note: !! will return true or false instead of DLL
+         return this.unshift(val);
+        }
+
+        if(index === this.length){
+            return this.push(val);
+         }
+        
+        var prevNode = this.get(index - 1);
+        var nextNode = prevNode.next;
+
+        prevNode.next = newNode,  newNode.prev = prevNode;
+        newNode.next = nextNode,  nextNode.prev = newNode;
+        this.length++;
+        return true;
+        
+    }
+
+    remove(index){
+
+        if(index < 0 || index >= this.length)
+            return false;
+
+        if(index === 0){
+           return this.shift();
+        }
+
+        if(index === this.length -1){
+           return this.pop();
+        }
+
+        var foundNode = this.get(index);
+        var prevNode = foundNode.prev;
+        var nextNode = foundNode.next;
+
+        prevNode.next = nextNode, nextNode.prev = prevNode;
+        foundNode.next = null, foundNode.prev = null;  //This is to remove links for prev and next
+        this.length--;
+        return foundNode;
+        
+    }
     
 }
 
 var first = new Node(12);
 first.next = new Node(13);
 first.next.prev = first;
-console.log(first);
+//console.log(first);
+var list = new DoublyLinkedList();
+
+list.push("srinivas");
+list.push("nalla");
+list.push("test");
+
+
+
+//console.log(list.set(2, "demo"))
+
+console.log(list.remove(1));
+
+//console.log(list);
 
